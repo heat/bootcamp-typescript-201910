1b37786e6f1d4a805f6b77b158c51a15469b5868
Onezin Moreira <zinogabriel@gmail.com>
2019-10-26 04:15:37 +0000

    refatora step-3

--- src/components/fancy-button.tsx
+++ src/components/fancy-button.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 
 
 type FancyButtonProps = {
-    onAction: () => void;
+    onAction: () => Promise<void>;
     text?: string;
 }
 
-export class FancyButton extends React.Component<FancyButtonProps, {}> {
+type FancyButtonState = {
+    loading: boolean;
+}
+export class FancyButton extends React.Component<FancyButtonProps, FancyButtonState> {
 
-    processClick(e: React.MouseEvent) {
-        this.props.onAction();
+    state: FancyButtonState = {
+        loading: false,
+    }
+    async processClick(e: React.MouseEvent) {
+        try {
+            this.setState({ loading: true });
+            await this.props.onAction();
+        } catch (error) {
+            alert(error.message);
+        } finally {
+            this.setState({ loading: false });
+        }
     }
 
     render() {
         
         return (
-            <button className="button" onClick={ e => this.processClick(e)} >
+            <button className="button" onClick={ e => this.processClick(e) } disabled={this.state.loading} >
                 {this.props.text || 'MAIS'}
             </button>
         )
